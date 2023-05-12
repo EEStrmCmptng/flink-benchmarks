fb1b3204701879e1f7511b610ae59b4c1fbc9b9f
pentium3 <celerond@msn.com>
2023-05-12 18:29:09 -0400

    Imgproc query

---
+++ src/main/java/flink/queries/Imgproc.java
@@ -0,0 +1,93 @@
+package flink.queries;
+
+import flink.sources.ImagesDataGeneratorSource;
+import flink.functions.ImgTransformFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import flink.sinks.DummyLatencyCountingSink;
+import org.apache.flink.api.java.typeutils.GenericTypeInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
+import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import javax.annotation.Nullable;
+
+import java.util.*;
+
+public class Imgproc{
+    private static final Logger logger  = LoggerFactory.getLogger(Imgproc.class);
+
+    public static void main(String[] args) throws Exception {
+        // Checking input parameters
+        final ParameterTool params = ParameterTool.fromArgs(args);
+        final int inputRatePerProducer = params.getInt("inputRate", 620);
+        final int imgSize = params.getInt("imgSize", 128);
+        final int batchSize = params.getInt("batchSize", 1);
+        final int experimentTimeInSeconds = params.getInt("experimentTimeInSeconds", 1800);
+        final int psrc = params.getInt("psrc", 1);
+        final int ptra = params.getInt("ptra", 2);
+        final int blurstep = params.getInt("blurstep", 2);
+        final int warmUpRequestsNum = params.getInt("warmUpRequestsNum", 0);
+        // --inputRate 640 --imgSize 128 --batchSize 1 --blurstep 2 --psrc 3 --ptra 6
+
+        Configuration configuration = new Configuration();
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(configuration);
+
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.getConfig().setAutoWatermarkInterval(1000);
+
+        //env.disableOperatorChaining();
+
+        // enable latency tracking
+        env.getConfig().setLatencyTrackingInterval(10000);
+
+        // Read the input data stream
+        System.out.println("Input producers: " + psrc);
+        System.out.println("Input rate per producer: " + inputRatePerProducer);
+
+        DataStream<Tuple2<ArrayList<ArrayList<Float>>, Long>> batches = env
+                .addSource(
+                        new ImagesDataGeneratorSource(batchSize, experimentTimeInSeconds, warmUpRequestsNum, inputRatePerProducer, imgSize))
+                .setParallelism(psrc).slotSharingGroup("src")
+                .name("Bids Source").uid("Bids-Source");
+                //.assignTimestampsAndWatermarks(new TimestampAssigner())
+                //.name("TimestampAssigner");
+
+        SingleOutputStreamOperator<Tuple2<ArrayList<ArrayList<Float>>, Long>> transformed = batches
+                .process(new ImgTransformFunction(blurstep, imgSize))
+                .setParallelism(ptra).slotSharingGroup("tra").name("Mapper");
+
+
+        GenericTypeInfo<Object> objectTypeInfo = new GenericTypeInfo<>(Object.class);
+        transformed.transform("DummyLatencySink", objectTypeInfo, new DummyLatencyCountingSink<>(logger))
+                .setParallelism(1)
+                .name("Latency Sink")
+                .uid("Latency-Sink")
+                .slotSharingGroup("sink");
+
+        System.out.println(env.getExecutionPlan());
+        env.execute("Imgproc");
+    }
+
+private static final class TimestampAssigner implements AssignerWithPeriodicWatermarks<Tuple2<ArrayList<ArrayList<Float>>, Long>> {
+        private long maxTimestamp = Long.MIN_VALUE;
+
+        @Nullable
+        @Override
+        public Watermark getCurrentWatermark() {
+            return new Watermark(maxTimestamp);
+        }
+
+        @Override
+        public long extractTimestamp(Tuple2<ArrayList<ArrayList<Float>>, Long> element, long previousElementTimestamp) {
+            maxTimestamp = Math.max(maxTimestamp, element.f1);
+            return element.f1;
+        }
+    }
+}
+
---
+++ src/main/java/flink/sources/ImagesDataGeneratorSource.java
@@ -0,0 +1,47 @@
+package flink.sources;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
+
+import java.util.*;
+
+public class ImagesDataGeneratorSource extends RichParallelSourceFunction<Tuple2<ArrayList<ArrayList<Float>>, Long>> {
+    private final ImagesDataGenerator generator;
+    private volatile boolean running = true;
+    private long eventsCountSoFar = 0;
+    private long experimentTimeInSeconds = 0;
+    private final int inputRate;
+
+    public ImagesDataGeneratorSource(int batchSize, int experimentTimeInSeconds, int warmupRequestsNum, int inputRate, int imgSize) {
+        this.generator = new ImagesDataGenerator(batchSize, experimentTimeInSeconds, warmupRequestsNum, imgSize);
+        this.inputRate=inputRate;
+        this.experimentTimeInSeconds=experimentTimeInSeconds;
+    }
+
+    @Override
+    public void run(SourceContext<Tuple2<ArrayList<ArrayList<Float>>, Long>> sourceContext) throws Exception {
+
+        Date finishTime = new Date();
+
+        while (running && new Date().getTime() - finishTime.getTime() < this.experimentTimeInSeconds*1000) {
+            long emitStartTime = System.currentTimeMillis();
+
+            for (int i = 0; i < this.inputRate; i++) {
+                sourceContext.collect(this.generator.next());
+                eventsCountSoFar++;
+            }
+
+            // Sleep for the rest of time slice if needed
+            long emitTime = System.currentTimeMillis() - emitStartTime;
+            if (emitTime < 1000) {
+                Thread.sleep(1000 - emitTime);
+            }
+        }
+
+    }
+
+    @Override
+    public void cancel() {
+        running = false;
+    }
+}
